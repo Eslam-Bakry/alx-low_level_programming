59718c88dbb9dfb3f137d70b4ff60ad9bbc6bcc9
Eslam Bakry <eslambakrydev@gmail.com>
2024-06-21 16:45:39 -0700

    tast2t1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+for (char character = 'a'; character <= 'z'; character++)
+{
+putchar(character);
+}
+putchar('\n');
+return (0);
+}
