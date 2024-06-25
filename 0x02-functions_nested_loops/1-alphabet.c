bfc62e34effb556091939c16cc498e3c9c741f8d
EslamBakry <eslambakrydev@gmail.com>
2024-06-26 01:04:23 +0300

    task1t1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int n;
+
+for (n = 0; n < 29; n++)
+{
+	_putchar("abcdefghijklmnopqrstuvwxyz"[n]);
+}
+_putchar('\n');
+return (0);
+}
