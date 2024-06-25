387c9a279cd6a86b7a695af590b691722c2ff880
EslamBakry <eslambakrydev@gmail.com>
2024-06-26 02:19:26 +0300

    task2t2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* print_alphabet - Entry point
+* print_alphabet_x10 - Entry point
 *
 * Return: Always 0 (Success)
 */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 int n;
 int i;
