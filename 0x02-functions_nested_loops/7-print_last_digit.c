3a5144696669722e1381d600d8a6149ca6822687
EslamBakry <eslambakrydev@gmail.com>
2024-06-26 03:40:42 +0300

    task7t4

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,7 @@ int print_last_digit(int c)
 int lastDigit = c % 10;
 if (lastDigit < 0)
 {
-    lastDigit = lastDigit * -1;
+lastDigit = lastDigit * -1;
 }
 _putchar('0' + lastDigit);
 return (lastDigit);
