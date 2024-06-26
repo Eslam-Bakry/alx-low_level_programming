3b7de7a37f73ec826f85ee58ce91ecc226907ca0
EslamBakry <eslambakrydev@gmail.com>
2024-06-26 03:03:05 +0300

    task4t2

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* _islower - Entry point
+* _isalpha - Entry point
 * @c: the character to be checked
 * Return: Always 0 (Success)
 */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
 {
