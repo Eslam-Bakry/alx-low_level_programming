ef69a8de58953721c7990fea657f41fb08e7bce4
EslamBakry <eslambakrydev@gmail.com>
2024-06-26 03:12:02 +0300

    task5t1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_sign - Entry point
+* @n: the character to be checked
+* Return: Always 0 (Success)
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+_putchar("+");
+}
+else if (n < 0)
+{
+return (-1);
+_putchar("-");
+}
+else
+{
+return (0);
+_putchar("0");
+}
+}
