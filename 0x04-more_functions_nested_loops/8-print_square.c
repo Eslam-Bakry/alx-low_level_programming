956dcb5ad08b8ff8f252d139668741675a239c3d
EslamBakry <eslambakrydev@gmail.com>
2024-06-27 15:22:48 +0300

    task8t2

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,30 @@
 #include "main.h"
 
 /**
-* main - check the code
-*
-* Return: Always 0.
+* print_square - Entry point
+* @size: size of the square
+* Return: Always 0 (Success)
 */
+void print_square(int size)
+{
+	int height = size;
+	int width = size;
+	int height_counter;
+	int width_counter;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (height_counter; height_counter < height; height_counter++)
+		{
+			for (width_counter; width_counter < width; width_counter++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
