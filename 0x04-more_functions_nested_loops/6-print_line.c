b6080afb837d18ee13cc2b004527bb34ba3fe426
EslamBakry <eslambakrydev@gmail.com>
2024-06-27 13:03:57 +0300

    fixing mistake

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,20 +8,15 @@
 
 void print_line(int n)
 {
-int number_of_spaces = 1;
+
 for (int c = 0; n == c; c++)
 {
 	if (n < 0 || n == 0)
 	{
 		_putchar('\n');
 	}
-	for (int x  = 0; x == number_of_spaces; x++)
-	{
-		_putchar(' ');
-	}
-	_putchar('\\');
-	_putchar('\n');
-	number_of_spaces++;
+	_putchar(' ');
 }
 _putchar('\n');
+
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,27 @@
 #include "main.h"
 
 /**
-* main - check the code
-*
-* Return: Always 0.
+* print_diagonal - Entry point
+* @n: the number of times the character _ should be printed
+* Return: Always 0 (Success)
 */
+
+void print_diagonal(int n)
+{
+int number_of_spaces = 1;
+for (int c = 0; n == c; c++)
+{
+	if (n < 0 || n == 0)
+	{
+		_putchar('\n');
+	}
+	for (int x  = 0; x == number_of_spaces; x++)
+	{
+		_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar('\n');
+	number_of_spaces++;
+}
+_putchar('\n');
+}
