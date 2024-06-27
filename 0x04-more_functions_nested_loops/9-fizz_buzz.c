50b21e9ff743e0d9d75a0a8457b1e74438d6fda3
EslamBakry <eslambakrydev@gmail.com>
2024-06-27 16:01:43 +0300

    task9t1

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,34 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * main - check the code
 *
 * Return: Always 0.
 */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 101; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
