b00a20d2505353c0c26de6ee0a11314ffcf93e8a
EslamBakry <eslambakrydev@gmail.com>
2024-06-28 05:19:17 +0300

    task10t1

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,33 @@
 #include "main.h"
 
 /**
-* main - check the code
-*
-* Return: Always 0.
+* print_triangle - Entry point
+* @size: size of the triangle
+* Return: Always 0 (Success)
 */
+
+void print_triangle(int size)
+{
+	int counter1, counter2, counter3;
+	int spaces = size;
+	int hashs = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (counter1 = 0; counter1 < size; counter1++)
+	{
+		for (counter2 = size - counter1; counter2 > 1; counter2--)
+		{
+			_putchar(' ');
+		}
+		for (counter3 = 0; counter3 <= counter1; counter3++)
+		{
+			_putchar('#');
+		}
+	}
+	}
+}
