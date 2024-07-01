eb5a77292d99ddeaf00dfadd828d291ef2eafbc2
EslamBakry <eslambakrydev@gmail.com>
2024-07-02 01:08:46 +0300

    task3t2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
 /**
  * _puts - resets the value of an int var to 98
  * @str: string to be printed
- *
+ * Return: On success 0
  */
 
 void _puts(char *str)
@@ -13,6 +13,8 @@ int i  = 0;
 while (*(str + i) != '\0')
 {
 	_putchar(*(str + i));
+	i++;
 }
 _putchar('\n');
+return (0);
 }
