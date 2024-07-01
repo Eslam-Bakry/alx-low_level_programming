26b362efc2e439dc27cdb5b1f0812753e08bb156
EslamBakry <eslambakrydev@gmail.com>
2024-07-02 00:35:19 +0300

    task2t1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - resets the value of an int var to 98
+ * @s: string to be mesuared
+ * Return: the value of i
+ */
+
+int _strlen(char *s)
+{
+
+int i  = 0;
+for (i; s[i] != '\0'; i++)
+{
+	i = i;
+}
+return (i);
+
+}
