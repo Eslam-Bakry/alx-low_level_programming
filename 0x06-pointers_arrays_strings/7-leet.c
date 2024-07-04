5ac9b44d22217548660862900289c418040d6fa9
EslamBakry <eslambakrydev@gmail.com>
2024-07-05 02:19:52 +0300

    task7t1

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,32 @@
 #include "main.h"
 
 /**
- * *leet - check codes
- * @str: string to be printed
- * Return: On success 0
+ * *leet - check code
+ * @str: string to be checked
+ * Return: On success str
  */
 
-char *leet(char *)
+char *leet(char *str)
 {
+	char *ptr = str;
+	char leet_map[] = "aAeEoOtTlL";
+	char leet_replace[] = "4433007711";
+	int i;
 
+	while (*ptr)
+	{
+		i = 0;
+		while (i < 10)
+		{
+			if (*ptr == leet_map[i])
+			{
+				*ptr = leet_replace[i];
+				break;
+			}
+			i++;
+		}
+		ptr++;
+	}
+
+	return (str);
 }
