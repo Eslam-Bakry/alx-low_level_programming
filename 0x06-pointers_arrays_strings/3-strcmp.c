20be9515b4b8371049ce58367830b05541acaabb
EslamBakry <eslambakrydev@gmail.com>
2024-07-05 01:54:27 +0300

    task4t1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * _strcmp - check codes
- * @str: string to be printed
- * Return: On success 0
+ * _strcmp - check code
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ * Return: On success dest
  */
+
 
 int _strcmp(char *s1, char *s2)
 {
-
+while (*s1 == *s2 && *s1 != '\0')
+{
+	s1++;
+	s2++;
+}
+return (*s1 - *s2);
 }
