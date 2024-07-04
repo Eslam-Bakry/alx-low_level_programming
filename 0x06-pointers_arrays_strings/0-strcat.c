b311fdc06bf3d1ad4c074f959980db56298f9624
EslamBakry <eslambakrydev@gmail.com>
2024-07-05 01:30:26 +0300

    task0t1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,11 +2,29 @@
 
 /**
  * *_strcat - check codes
- * @str: string to be printed
+ * @dest: first string to be concatenated
+ * @src: second string to be concatenated
  * Return: On success 0
  */
 
 char *_strcat(char *dest, char *src)
 {
+char *d = dest;
+const char *s = src;
 
+while (*d != '\0')
+{
+	d++;
+}
+
+while (*s != '\0')
+{
+	*d = *s;
+	d++;
+	s++;
+}
+
+*d = '\0';
+
+return (dest);
 }
