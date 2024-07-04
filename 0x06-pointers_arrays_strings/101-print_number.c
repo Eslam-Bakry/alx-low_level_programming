f1a5735e0d2cc6c84e21ca098e0215e0bdae1e22
EslamBakry <eslambakrydev@gmail.com>
2024-07-05 02:26:20 +0300

    task9t1

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - check code
+ * @n: number to be printed
+ * Return: On success n
+ */
+
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+}
