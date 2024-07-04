32547181c965cf6e2fc3679b48fa3b8d6a690a1e
EslamBakry <eslambakrydev@gmail.com>
2024-07-05 01:50:50 +0300

    task3t1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,26 @@
 #include "main.h"
 
 /**
- * *_strncpy - check codes
- * @str: string to be printed
- * Return: On success 0
+ * *_strncpy - check code
+ * @dest: where the string is going to be stored
+ * @src: the string to be copied
+ * @n: number of times to copy the string
+ * Return: On success dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
+int i;
 
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+	dest[i] = src[i];
+}
+
+for (; i < n; i++)
+{
+	dest[i] = '\0';
+}
+
+return (dest);
 }
