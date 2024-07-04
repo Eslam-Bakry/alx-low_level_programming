930bc9c0f140e328f670fa5b30e615727d7b0e43
EslamBakry <eslambakrydev@gmail.com>
2024-07-05 02:01:15 +0300

    task4t1

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
 /**
- * reverse_array - check codes
- * @str: string to be printed
- * Return: On success 0
+ * reverse_array - check code
+ * @a: the string to be reversed
+ * @n: the number of elements of the array
+ * Return: On success dest
  */
 
 void reverse_array(int *a, int n)
 {
+int start = 0;
+int end = n - 1;
+int temp;
 
+while (start < end)
+{
+	temp = a[start];
+	a[start] = a[end];
+	a[end] = temp;
+	start++;
+	end--;
+}
 }
