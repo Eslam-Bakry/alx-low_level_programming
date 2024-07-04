fed4b2f7de4b7ad0f31a2d85b17a78b83970f0c7
EslamBakry <eslambakrydev@gmail.com>
2024-07-05 02:05:23 +0300

    task5t1

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,11 +2,22 @@
 
 /**
  * *string_toupper - check codes
- * @str: string to be printed
- * Return: On success 0
+ * @str: string to be changed
+ * Return: On success str
  */
 
-char *string_toupper(char *)
+char *string_toupper(char str)
 {
+char *ptr = str;
 
+while (*ptr != '\0')
+{
+	if (*ptr >= 'a' && *ptr <= 'z')
+	{
+		*ptr = *ptr - 32;
+	}
+	ptr++;
+}
+
+return (str);
 }
